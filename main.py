d57b4beeb87d8c9e67c15592da5c9c72f21eaadc
Mohammedehap1 <138616768+Mohammedehap1@users.noreply.github.com>
2023-08-10 13:57:27 -0700

    update
    worke data_fun good
    ''         cf chack good
    ''

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ import time
 class data_function:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.data_file = 'test.txt'
+        self.data_file = 'data.txt'
+        self.data_list = []
+        self.token = None
         self.token_list = None
         self.token_file = None 
         self.account_name = None
@@ -19,17 +21,18 @@ class data_function:
         self.banned = False
     
     def write_data(self,*args):
-        file = open(self.token_file,'a')
-        file.write(f'{self.token} / /{self.account_name} / /{self.last_claim} / /{self.current_coin ,self.banned}')
+        file = open(self.data_file,'a')
+        file.write(f'{self.token} / /{self.account_name} / /{self.last_claim} / /{self.current_coin} / /{self.banned}\n')
         file.close()
     def read_data(self,*args):
         data_file = open(self.data_file,'r')
         data_lines = data_file.readlines()
         for line in range(len(data_lines)):
             data = data_lines[line].split('\n')
-            self.data_list.append(data)
-    def make_r_data(self,index = 0,*args):
-        data = self.data_list[index].split('\n')
+            self.data_list.append(data[0])
+    def make_data_readble(self,index = 0,*args):
+        data = self.data_list[index].split(' / /')
+        self.token, self.account_name, self.last_claim, self.current_coin, self.banned = data[0], data[1], data[2], data[3], data[4]
         
         
     
@@ -49,7 +52,7 @@ class Browser:
         self.options = webdriver.ChromeOptions()
         self.options.add_argument(f'user-agent={self.user_agent}')
         self.options.add_argument(f'user-data-dir={self.user_data}')
-        self.driver = uc.Chrome( options=self.options)
+        self.driver = uc.Chrome(executable_path=self.driver_path ,options=self.options)
     def get_tokens(self,token_path,*args):
         token_file = open(token_path,'r')
         tokens = token_file.readlines()
@@ -59,7 +62,7 @@ class Browser:
             self.tokens.append(token[0])
     def login_discord(self, *args):
       self.driver.execute_script('window.t = "' + self.token + '";window.localStorage = document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage;window.setInterval(() => window.localStorage.token = `"${window.t}"`); window.location.reload();')
-    def load_element(self, element, key = '', click = False):
+    def load_element(self, element, key = '', click = False, callback = False):
       found = False
       while not found:
         time.sleep(.5)
@@ -69,20 +72,44 @@ class Browser:
             self.driver.find_element(By.XPATH,element).click()
           elif key != '' :
             self.driver.find_element(By.XPATH,element).send_keys(key)
+          elif callback == True:
+             return True
           found = True
         except:
           found = False
+          if callback == True:
+             return False
     def change_window(self, *args):
       all_handles=self.driver.window_handles
       self.home_page = self.driver.current_window_handle
       for handle in all_handles:
         if handle != all_handles:
           self.driver.switch_to.window(handle)
-    def check_cf(self,*args):
-      pass
+    def check_cf(self, xpath = '//*[@id="challenge-running"]',*args): #return True if pass cf 
+      found = 0
+      while True:
+        if self.load_element(xpath,callback=True) == True and found <= 3 :
+            time.sleep(20)
+            found +=1
+        elif self.load_element(xpath,callback=True) == True and found >3 :
+            return False
+        else:
+            return True
+  
+           
     def start(self, *args):
       print(self.driver_path)
-      self.driver.get("http://www.example.com/")
-      input()
+      self.driver.get("http://www.probot.io/")
+      check = self.check_cf()
 
-Browser().start()
+      input()
+class test(data_function):
+   def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.read_data()
+        print(self.data_list)
+        self.make_data_readble()
+        print(type(int(self.last_claim)))
+test()
+
+
