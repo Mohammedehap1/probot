e91f3f0da7b490ff7b1b74401be1d70639026938
Mohammedehap1 <medo2medo153@gmail.com>
2023-08-10 10:43:53 -0700

    ffgg

---
+++ main.py
@@ -0,0 +1,98 @@
+from selenium import webdriver
+import undetected_chromedriver as uc
+from fake_useragent import UserAgent as ua 
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import random
+import os
+class data_function:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.data_file = 'test.txt'
+        self.token_list = None
+        self.token_file = None 
+        self.account_name = None
+        self.last_claim = None
+        self.current_coin = None
+        self.banned = False
+    
+    def write_data(self,*args):
+        file = open(self.token_file,'a')
+        file.write(f'{self.token} / /{self.account_name} / /{self.last_claim} / /{self.current_coin ,self.banned}')
+        file.close()
+    def read_data(self,*args):
+        data_file = open(self.data_file,'r')
+        data_lines = data_file.readlines()
+        for line in range(len(data_lines)):
+            data = data_lines[line].split('\n')
+            self.data_list.append(data)
+    def make_r_data(self,index = 0,*args):
+        data = self.data_list[index].split('\n')
+        
+        
+    
+    
+    
+class Browser:
+    tokens = []
+    token_path = 'token.txt'
+    driver_path = os.getcwd() + "\\chromedriver.exe"
+    user_data = r"C:\Users\Admin\AppData\Local\Google\Chrome\User Data"
+    sshome_page = None 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.tokens == []:
+          self.get_tokens(self.token_path)
+        self.user_agent = ua().random
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument(f'user-agent={self.user_agent}')
+        self.options.add_argument(f'user-data-dir={self.user_data}')
+        self.driver = uc.Chrome(executable_path=self.driver_path, options=self.options)
+    def get_tokens(self,token_path,*args):
+        token_file = open(token_path,'r')
+        tokens = token_file.readlines()
+        token_file.close()
+        for count in range(len(tokens)):
+            token = tokens[count].split('\n')
+            self.tokens.append(token[0])
+    def login_discord(self, *args):
+      self.driver.execute_script('window.t = "' + self.token + '";window.localStorage = document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage;window.setInterval(() => window.localStorage.token = `"${window.t}"`); window.location.reload();')
+    def load_element(self, element, key = '', click = False):
+      found = False
+      while not found:
+        time.sleep(.5)
+        try:
+          self.driver.find_element(By.XPATH,element)
+          if click == True :
+            self.driver.find_element(By.XPATH,element).click()
+          elif key != '' :
+            self.driver.find_element(By.XPATH,element).send_keys(key)
+          found = True
+        except:
+          found = False
+    def change_window(self, *args):
+      all_handles=self.driver.window_handles
+      self.home_page = self.driver.current_window_handle
+      for handle in all_handles:
+        if handle != all_handles:
+          self.driver.switch_to.window(handle)
+    def check_cf(self,*args):
+      pass
+
+    '''
+    def captcha_solver(self,*args):
+      iframes = self.driver.find_elements_by_tag_name('iframe')
+      for index in range(len(iframes)):
+        self.driver.switch_to.default_content()
+        iframe= self.driver.find_elements_by_tag_name('iframe')[index]
+        self.driver.switch_to.frame(iframe)
+        self.driver.implicitly_wait(random.randint(2, 3))
+        try:
+          audioBtn = self.driver.find_element_by_id("recaptcha-audio-button")
+          audioBtn.click()
+          audioBtnFound = True
+          audioBtnIndex = index
+          break
+        except Exception as e:
+          pass
+        '''
